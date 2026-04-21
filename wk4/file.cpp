22bbcbd942cc4ac85087fb2945c7cf09c4965a3b
WispyV <lenap2006@att.net>
2026-04-21 12:09:07 -0500

    added more files and names and you can find files and more names

--- wk4/file.cpp
+++ wk4/file.cpp
@@ -7,11 +7,10 @@
 
 using namespace std;
 
-int main() {
-    cout <<"let's write to a file!\n";
-    
+void read(string path = "save.txt") {
+
     string line;
-    ifstream readFile("save.txt");  /// create a new vaar, and try to open a file
+    ifstream readFile("path");  /// create a new vaar, and try to open a file
     if(readFile.is_open()) {
         cout <<"The file is open.\n";
         while(getline(readFile, line)) {
@@ -25,19 +24,78 @@ int main() {
 
     readFile.close();
 
-    cout <<"what names would you like?\n";
-    cout <<"enter an empty line to stop.\n";
+}
+
+void read(vector<string>& vec, string path = "save.txt") {
+    string line;
+    ifstream readFile(path);
+    if(readFile.is_open()) {
+        cout <<"The file is open.\n";
+        while(getline(readFile, line)) {
+            vec.push_back(line);
+        }
+
+    }
+    else {
+        cout <<"couldn't open that file.\n";
+    }
+
+    readFile.close();
+
+}
+
+void write(vector<string>& vec, string path = "save.txt") {
+    // cout <<"starting write() function.\n";
+    ofstream writeFile("path");
+
+    if(writeFile.is_open()) {
+        // cout <<"the write file is open.\n";
+        for(int i = 0; i <vec.size(); i++) {
+            writeFile <<vec[i] << endl;
+        }
+    }
+    else {
+        cout <<"couldn't open new writeFile.\n";
+    }
+    writeFile.close();
+}
+
+int main() {
+    cout <<"let's write to a file!\n";
+    vector<string> favs;
     string input = "asdf";
 
-    ofstream writeFile("save.txt");
+
+    string path = "save.txt";
+
+    cout << "what file would you like to open?\n Enter to use default.\n";
+    getline(cin, input);
+    if(input !="no") {
+        path = input;
+    }
+
+    read(path);
+    read(favs, path); 
+
+
+    cout <<"what names would you like to add?\n";
+    cout <<"enter an empty line to stop.\n";
+
+    // ofstream writeFile("save.txt");
 
     while(input !="") {
         cout <<"> ";
         getline(cin, input);
-        writeFile << input << endl;
+        if(input == "") {
+            break;
+        }
+        // writeFile << input << endl;
+        favs.push_back(input);
     }
 
-    writeFile.close();
-    
+    write(favs, path);
+
+    // writeFile.close();
+
     return 0;
 }
